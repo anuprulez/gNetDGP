eaaf07c3cd1fe8f613e2d8fd47ae1b48819c560e
anuprulez <anup.rulez@gmail.com>
2024-02-28 09:26:43 +0100

    add .cpu

--- main.py
+++ main.py
@@ -414,7 +414,7 @@ def generic_predict(
         predicted_probs = F.softmax(
             model(gene_net_data, disease_net_data, x_in).clone().detach(), dim=1
         )[:, -1:]
-    input_tuples['Score'] = predicted_probs.numpy()
+    input_tuples['Score'] = predicted_probs.cpu().numpy()
 
     if sort_result_by_score:
         input_tuples.sort_values(by=['Score'], inplace=True, ascending=False)
@@ -830,7 +830,7 @@ def specific_predict(
         predicted_probs = F.softmax(
             model(gene_net_data, disease_net_data, x_in).clone().detach(), dim=1
         )[:, -1:]
-    input_genes['Score'] = predicted_probs.numpy()
+    input_genes['Score'] = predicted_probs.cpu().numpy() #predicted_probs.numpy()
 
     if sort_result_by_score:
         input_genes.sort_values(by=['Score'], inplace=True, ascending=False)
